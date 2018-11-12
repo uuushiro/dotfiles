143a9fc0890f01818c270569c3f8ce90fcddcc57
matsutani <yushiro.ma2ta2.21@gmail.com>
2018-11-12 19:37:07 +0900

    fix path

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.tmux.conf ~/.tmux.conf
+ln -sf ~/git/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/git/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/git/dotfiles/.tmux.conf ~/.tmux.conf
